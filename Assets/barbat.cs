d7495a466ce842f3d1a390efa9ac9978368be280
unknown <cosimo.zurlo96@gmail.com>
2023-04-13 18:11:24 +0300

    Clasa barbat acum se misca stanga dreapta sus jos mortii lui, si are si animatie

--- Assets/barbat.cs
+++ Assets/barbat.cs
@@ -5,42 +5,57 @@ using UnityEngine;
 public class barbat : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animate;
+    public float jumpSpeed = 5;
+    public float moveForce = 10;
+    public float maxMoveSpeed = 3;
 
-    private float time = 0.0f;
     private bool isMoving = false;
-    private bool isJumpPressed = false;
+
+    private const int MoveLeft = 0;
+    private const int MoveRight = 1;
+    private const int Jump = 2;
+    private bool[] actions = { false, false, false };
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     void Update()
     {
-        isJumpPressed = Input.GetKeyDown(KeyCode.Space);
+        animate.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        actions[MoveLeft]  = (actions[MoveLeft]  || Input.GetKeyDown(KeyCode.A)) && !Input.GetKeyUp(KeyCode.A);
+        actions[MoveRight] = (actions[MoveRight] || Input.GetKeyDown(KeyCode.D)) && !Input.GetKeyUp(KeyCode.D);
+        actions[Jump]      =  actions[Jump]      || Input.GetKeyDown(KeyCode.W);
     }
 
     void FixedUpdate()
     {
-        if (isJumpPressed)
+        if (actions[MoveLeft])  { rb.AddForce(new Vector2(-moveForce * 10, 0)); }
+        if (actions[MoveRight]) { rb.AddForce(new Vector2(moveForce * 10, 0)); }
+
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxMoveSpeed, maxMoveSpeed), rb.velocity.y);
+
+
+        if (actions[Jump])
         {
-            // the cube is going to move upwards in 10 units per second
-            rb.velocity = new Vector3(0, 3, 0);
-            isMoving = true;
-            Debug.Log("jump");
+            actions[Jump] = false;
+
+            if (!isMoving)
+            {
+                // the cube is going to move upwards in 10 units per second
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                isMoving = true;
+                Debug.Log("jump");
+            }
         }
 
-        if (isMoving)
+        if (rb.velocity.y == 0)
         {
-            // when the cube has moved for 10 seconds, report its position
-            time = time + Time.fixedDeltaTime;
-            if (time > 10.0f)
-            {
-                Debug.Log(gameObject.transform.position.y + " : " + time);
-                time = 0.0f;
-            }
+            isMoving = false;
         }
     }
 }
